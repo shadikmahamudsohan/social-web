d892df9e9bb876ee88cb00b6b04dbc9c98961a06
shadik <shadikmahamudsohan@gmail.com>
2022-06-17 15:22:37 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { Button } from '@mui/material';
+import { makeStyles } from '@mui/styles'
 import './App.css';
+import PersonIcon from '@mui/icons-material/Person';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    background: theme.palette.success.dark,
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button variant="contained" size='large' startIcon={<PersonIcon />}
+        className={classes.root}
+      >Contained</Button>
     </div>
   );
 }
